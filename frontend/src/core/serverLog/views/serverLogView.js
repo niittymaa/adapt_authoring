85eeebad1e8733619c7278228b2fdc4121d35a49
Thomas Taylor <thomas.taylor@kineo.com>
2016-05-27 15:08:23 +0100

    Remove unused imports

--- frontend/src/core/serverLog/views/serverLogView.js
+++ frontend/src/core/serverLog/views/serverLogView.js
@@ -1,9 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
-  var Backbone = require('backbone');
-  var Handlebars = require('handlebars');
   var OriginView = require('coreJS/app/views/originView');
-  var Origin = require('coreJS/app/origin');
 
   var ServerLogView = OriginView.extend({
     tagName: 'div',
